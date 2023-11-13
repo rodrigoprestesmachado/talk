aafe63815151d7492b05f493816c1cb0114f50f5
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2023-11-13 17:18:18 -0300

    Front end #5

--- front/cypress/e2e/createUser.cy.js
+++ front/cypress/e2e/createUser.cy.js
@@ -10,13 +10,13 @@ describe('template spec', () => {
     cy.get('#password').clear();
     cy.get('#password').type('12345678');
     cy.get('.el-col-10 > .el-button > span').click();
-    cy.get('.el-col-10').click();
-    cy.get('.el-col-10').click();
+    cy.get('.el-col-10').click({force: true});
+    cy.get('.el-col-10').click({force: true});
     cy.get('.el-col-10', { timeout: 30000 }).should('be.visible');
     cy.get('#channelName').clear('c');
     cy.get('#channelName').type('canal');
     cy.get('span').click({ multiple: true });
-    cy.get('.el-col-20').click();
+    cy.get('.el-col-20').click({force: true});
     cy.get('.el-col-20 > :nth-child(2)').should('have.text', 'Canal: canal');
   });
 })
